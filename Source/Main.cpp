aaa4209443678aea7391504d3312d22c82db3736
William Sharkey <williamsharkey@gmail.com>
2019-10-29 03:02:00 -0400

    add example commands

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,12 +1,44 @@
-#include "../JuceLibraryCode/JuceHeader.h"
+﻿#include "../JuceLibraryCode/JuceHeader.h"
 #include "DroneGen.h"
 #include "rang.h"
 #define LIBCMDF_IMPL
-#include "libcmdf.h"
+#include "libcmdf.h" 
+
+
+#define PROG_INTRO "test - A simple test program for libcmdf.\n" \
+	"You can use this as a reference on how to use the library!"
+#define PRINTARGS_HELP "This is a very long help string for a command.\n" \
+                       "As you can see, this is concatenated properly. It's pretty good!"
+static CMDF_RETURN do_hello(cmdf_arglist* arglist) {
+	std::cout << "Hello, world!" << std::endl;
+
+	return CMDF_OK;
+}
+
+static CMDF_RETURN do_printargs(cmdf_arglist* arglist) {
+	if (!arglist) {
+		std::cout << "No arguments provided!" << std::endl;
+		return CMDF_OK;
+	}
+
+	std::cout << "Total arguments = " << arglist->count << std::endl;
+	for (size_t i = 0; i < arglist->count; i++)
+		std::cout << "Argument " << i << ": \'" << arglist->args[i] << "\'" << std::endl;
+
+	std::cout << std::endl;
+
+	return CMDF_OK;
+}
+
 
 int main(int argc, char* argv[])
 {
-	cmdf_init("hello> ", "world", "yo", "hi", 3, 1);
+	
+	cmdf_init( " sndedit > ", " welcome to sndedit", PROG_INTRO, "hi", '-', 1);
+
+	cmdf_register_command(do_hello, "hello", "prints hello world");
+	cmdf_register_command(do_printargs, "printargs", PRINTARGS_HELP);
+
 	cmdf_commandloop();
 
 	juce::initialiseJuce_GUI();
@@ -15,5 +47,7 @@ int main(int argc, char* argv[])
 	juce::shutdownJuce_GUI();
 	return 0;
 }
+
+
 
 //wm was here
